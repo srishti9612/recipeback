8dfe10ae0274203b060ef161d19c82fe60d9336f
Srishti Sharma <srishtishar@gmail.com>
2021-03-26 12:40:06 +0530

    Some changes.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const logger = require('./utils/logger')
 const server = http.createServer(app)
 console.log('inside index.js')
 
-const PORT = process.env.PORT || config.PORT
+//const PORT = process.env.PORT || config.PORT
 
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })

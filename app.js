2096a28a0f9c9168ea3f6658cf7ee88ab8bbedb5
Srishti Sharma <srishtishar@gmail.com>
2021-03-26 17:47:43 +0530

    Producton fixes

--- app.js
+++ app.js
@@ -44,5 +44,6 @@ app.use('/api/bookmark', bookmarkRouter)
 app.use('/api/updatedraft', draftRouters.updateDraft)
 app.use('/api/publishdraft', draftRouters.publishDraft)
 app.use('/api', deleteRouter)
+app.use('*', function (req, res, next) { res.sendFile('./build/index.html')})
 
 module.exports = app
